7c48d7543b06071323ad0189469d14b3670d724a
meghaboi <meghanadhpamidi@gmail.com>
2025-06-07 11:00:06 +0530

    ...

--- ui/ui_components.py
+++ ui/ui_components.py
@@ -1,4 +1,4 @@
-from components.chat_interface import display_chat_interface
-from components.evaluation_interface import display_evaluation_interface
+from ui.components.chat_interface import display_chat_interface
+from ui.components.evaluation_interface import display_evaluation_interface
 
 __all__ = ['display_chat_interface', 'display_evaluation_interface'] 
